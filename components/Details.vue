c23b8fc0674c8b1b8265e8fea7a28c9ecd3b15ed
Jonas <jonasparnow@gmail.com>
2018-07-03 14:03:55 +0200

    move detail item to component

--- components/Details.vue
+++ components/Details.vue
@@ -12,19 +12,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="line in matches" :class="{ 'warn' : !line['matches'].length }">
-          <td>
-            {{ line['input'] }}
-          </td>
-          <td>
-            <ol v-if="line['matches'].length">
-              <li v-for="match in line['matches']">
-                {{ match.match.label }} ({{ match.probability.toFixed(2) }}%)
-              </li>
-            </ol>
-            <span v-else>{{ line['message'] }}</span>
-          </td>
-        </tr>
+        <DetailsItem v-for="(match, id) in matches" :item="match" key="match.input" :id="id" />
       </tbody>
     </table>
   </section>
@@ -32,12 +20,16 @@
 
 <script>
   import { mapGetters } from 'vuex'
+  import DetailsItem from '~/components/DetailsItem.vue'
 
   export default {
     computed: {
       ...mapGetters([
         'matches'
       ])
+    },
+    components: {
+      DetailsItem
     }
   }
 </script>
---
+++ components/DetailsItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <tr :class="{ 'warn' : !item['matches'].length }">
+    <td>
+      {{ id }} {{ item['input'] }}
+    </td>
+    <td>
+      <ol v-if="item['matches'].length">
+        <li v-for="match in item['matches']">
+          {{ match.match.label }} ({{ match.probability.toFixed(2) }}%)
+        </li>
+      </ol>
+      <span v-else>{{ item['message'] }}</span>
+    </td>
+  </tr>
+</template>
+
+<script>
+  // import { mapState, mapActions } from 'vuex'
+
+  export default {
+    props: ['item', 'id']
+  }
+</script>
+
+<style lang="scss">
+
+</style>
